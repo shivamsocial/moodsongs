3ff8c0e3e493eebd3329777b0be2c2457470c621
Shivam <shivamupadhyaysocial@gmail.com>
2024-11-12 03:25:18 +0530

    Update SiteMap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,8 +1,25 @@
 export default function sitemap() {
-  const moods = ["Romantic", "Happy", "Sad", "Motivational", "Chill", "Party"];
+  const moods = [
+    { emoji: "😀", name: "Happy" },
+    { emoji: "💘", name: "Romantic" },
+    { emoji: "⚡", name: "EDM" },
+    { emoji: "🤘", name: "Hip-Hop" },
+    { emoji: "🌟", name: "Motivational" },
+    { emoji: "💪", name: "Rap" },
+    { emoji: "🎸", name: "Rock" },
+    { emoji: "🥳", name: "Party" },
+    { emoji: "🎷", name: "Jazz" },
+    { emoji: "😊", name: "Acoustic" },
+    { emoji: "🙏", name: "Folk" },
+    { emoji: "🕉️✝️", name: "Devotional" },
+    { emoji: "🧘‍♂️", name: "Meditation" },
+    { emoji: "👶", name: "Children" },
+    { emoji: "😌", name: "Relax Chill" },
+    { emoji: "📻", name: "Lofi" },
+  ];
 
   const urls = moods.map((mood) => ({
-    url: `https://www.moodsongs.net/video/${mood}?videoIndex=0`,
+    url: `https://www.moodsongs.net/video/${mood.name}?videoIndex=0`,
     lastModified: new Date(),
     changeFrequency: "daily",
     priority: 0.8,
@@ -12,7 +29,7 @@ export default function sitemap() {
   urls.unshift({
     url: "https://www.moodsongs.net/",
     lastModified: new Date(),
-    changeFrequency: "monthly",
+    changeFrequency: "daily",
     priority: 1.0,
   });
 
