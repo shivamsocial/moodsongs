ddd25554ad75e5e7ab5e2a236080187a49471e37
Shivam <shivamupadhyaysocial@gmail.com>
2025-01-10 19:04:32 +0530

    ss

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -18,15 +19,15 @@ class MyDocument extends Document {
         {/* Dynamically set the lang attribute */}
         <Head>
           {/* Google Analytics gtag.js Script */}
-          <script
+          <Script
             async
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6578867983427163"
             crossOrigin="anonymous"
-          ></script>
-          <script
+          ></Script>
+          <Script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=G-NLD3F8R8M8`}
-          ></script>
+          ></Script>
           <script
             dangerouslySetInnerHTML={{
               __html: `
